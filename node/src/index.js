c3caf1c59a5af574bbe09911d6173d764ebaad24
Gustavo Fadel <gustavo-959694@hotmail.com>
2023-08-03 23:38:56 -0300

    Feito ajustes no compose e adicionado o select no index.js

--- node/src/index.js
+++ node/src/index.js
@@ -3,15 +3,35 @@ const app = express()
 const port = 3000
 const sequelize = require('./database')
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   insertNameDatabase()
-  res.writeHead(200, {"Content-type":"text/plain"})
-  return res.end("<h1>Full Cycle Rocks!</h1>")
+  const listaHtmlPeople = await formatarRegistos()
+  res.writeHead(200, {"Content-type":"text/html"})
+  return res.end("<h1>Full Cycle Rocks!</h1>" + "</br>" + listaHtmlPeople)
 })
 
 sequelize.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
+
+async function formatarRegistos() {
+  const peoples = await obterTodosRegistrosPeople()
+  let lista = "<ul>"
+  peoples.forEach(element => {
+    let peopleAtual = `<li>${element.nome}</li>`
+    lista += peopleAtual
+  });
+  lista += "</ul>"
+  return lista
+}
+
+async function obterTodosRegistrosPeople(){
+  const sql = `SELECT * FROM people;`
+  const peoples = await sequelize.query(sql, {
+    type: sequelize.QueryTypes.SELECT
+  })
+  return peoples;
+}
 function insertNameDatabase(){
-  const nome = "João da Silva"
+  const nome = "Maria Joaquina"
   const sql = `INSERT INTO people (nome) VALUES ("${nome}");`
   sequelize.query(sql, {
     type: sequelize.QueryTypes.INSERT
